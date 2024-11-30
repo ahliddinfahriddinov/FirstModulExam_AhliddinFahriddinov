5031ab26fc4a2a8259f3597f4e76d2204908517f
unknown <Ahliddin dev>
2024-11-30 12:02:02 +0500

    exam

---
+++ Exam/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace Exam;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+
+
+    }
+
+    public static bool StartsWithPdp(string text)
+    {
+        if (text.StartsWith("PDP"))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public static string GetStartThree(string text)
+    {
+        if (text.Length < 3)
+        {
+            return text;
+        }
+        return text.Substring(0, 3);
+    }
+
+    public static List<int> GetFilledList(int length)
+    {
+        var digits = new List<int>();
+        for (int i = 0; i < length; i++)
+        {
+            Console.Write($"{i} : ");
+            var numbers = int.Parse(Console.ReadLine());
+            digits.Add(length);
+
+        }
+        return digits;
+
+    }
+    public static int GetEvenElements(List<int> list)
+    {
+        var counter = 0;
+        foreach (var element in list)
+        {
+            if (element % 2 == 0)
+            {
+                counter++;
+            }
+        }
+
+        return counter;
+    }
+    public static bool GetEvenElements(List<string> text)
+    {
+        foreach (var element in text)
+        {
+            if (element.Length > 5)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ Exam/Services/PhoneService.cs
@@ -0,0 +1,50 @@
+﻿using Exam.Models;
+namespace Exam.Services;
+public class PhoneService
+{
+    private List<Phone> phones;
+    public PhoneService()
+    {
+        phones = new List<Phone>();
+    }
+    public Phone AddPhone(Phone phone)
+    {
+        phone.Id = Guid.NewGuid();
+        phones.Add(phone);
+        return phone;
+    }
+    public Phone GetById(Guid id)
+    {
+        foreach (Phone phone in phones)
+        {
+            if (phone.Id == id)
+            {
+                return phone;
+
+            }
+        }
+        return null;
+    }
+    public bool DeletePhone(Phone deletedPhone)
+    {
+        var phone = GetById(deletedPhone.Id);
+        phones.Remove(phone);
+        return true;
+    }
+    public bool UpdatePhone(Phone updatedPhone)
+    {
+        var phone = GetById(updatedPhone.Id);
+
+        if (phone.Id == null)
+        {
+            var index = phones.IndexOf(phone);
+            phones[index] = phone;
+            return true;
+        }
+        return false;
+    }
+    public List<Phone> GetAllPhones()
+    {
+        return phones;
+    }
+}
